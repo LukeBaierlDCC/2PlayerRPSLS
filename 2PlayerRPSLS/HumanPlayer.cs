38d04aa6fcaa332603e30ed688ce62247b946774
LUKE BAIERL <lukeest1984@gmail.com>
2023-02-01 20:55:52 -0600

    Implemented the gameplay feature and added missing inheritance in HumanPlayer class.

--- 2PlayerRPSLS/HumanPlayer.cs
+++ 2PlayerRPSLS/HumanPlayer.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace _2PlayerRPSLS
 {
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
-        //public override Choice GetChoice()
-        //{
-        //    Console.WriteLine("");
-        //    string input = Console.ReadLine();
-        //    return (Choice)Enum.Parse(typeof(Choice), input);
-        //}
+        public override Choice GetChoice()
+        {
+            Console.WriteLine("Enter your choice (Rock, Paper, Scissors, Lizard, Snake):");
+            string input = Console.ReadLine();
+            return (Choice)Enum.Parse(typeof(Choice), input);
+        }
     }
 }
--- 2PlayerRPSLS/Program.cs
+++ 2PlayerRPSLS/Program.cs
@@ -10,8 +10,37 @@ namespace _2PlayerRPSLS
     {
         static void Main(string[] args)
         {
-            //Player player1 = new HumanPlayer { Name = "Player 1" };
+            Player player1 = new HumanPlayer { Name = "Player 1" };
 
+            Player player2 = new AIPlayer { Name = "AI Player" };
+
+            while (true)
+            {
+                Choice player1Choice = player1.GetChoice();
+                Choice player2Choice = player2.GetChoice();
+
+                Console.WriteLine("{0} chose {1}", player1.Name, player1Choice);
+                Console.WriteLine("{0} chose {1}", player2.Name, player2Choice);
+
+                if (player1Choice == player2Choice)
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+                else if ((player1Choice == Choice.Rock && (player2Choice == Choice.Scissors || player2Choice == Choice.Lizard)) ||
+                    (player1Choice == Choice.Paper && (player2Choice == Choice.Rock || player2Choice == Choice.Snake)) ||
+                    (player1Choice == Choice.Scissors && (player2Choice == Choice.Paper || player2Choice == Choice.Lizard)) ||
+                    (player1Choice == Choice.Lizard && (player2Choice == Choice.Paper || player2Choice == Choice.Snake)) ||
+                    (player1Choice == Choice.Snake && (player2Choice == Choice.Rock || player2Choice == Choice.Scissors)))
+                {
+                    Console.WriteLine("{0 wins!}", player1.Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} wins!", player2.Name);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
